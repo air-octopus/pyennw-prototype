6df25e85d4c47026930f13fe5c76d1519d46001b
Maks Ko <tttmaksko@mail.ru>
2018-02-20 14:53:28 +0300

    Add tables definitions for species, synapses and neurons

--- ennwdb.py
+++ ennwdb.py
@@ -15,11 +15,66 @@ class NeuralNetworkDB:
         """
 
         self.db = sql.connect(database_path)
-
-        pass
+        self._create_structure();
 
     def _create_structure(self):
-        
+        c = self.db.cursor()
+        c.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS nn_species_all (
+                id          INTEGER PRIMARY KEY,
+                parent_id   INTEGER,
+                is_alive    INTEGER
+            )
+            '''
+        )
+        c.execute(
+            '''
+            CREATE INDEX IF NOT EXISTS idx_nn_species_all_001 ON nn_species_all (
+                is_alive
+            )
+            '''
+        )
+
+        c.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS synapses (
+                id              INTEGER PRIMARY KEY,
+                species_id      INTEGER,
+                neuron_in_id    INTEGER,
+                neuron_owner_id INTEGER,
+                weight          NUMERIC
+            )
+            '''
+        )
+        c.execute(
+            '''
+            CREATE INDEX IF NOT EXISTS idx_synapses_001 ON synapses (
+                species_id
+            )
+            '''
+        )
+
+
+        c.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS neuron_bodies (
+                id                          INTEGER PRIMARY KEY,
+                species_id                  INTEGER,
+                transfer_function_type      INTEGER,
+                transfer_function_params    STRING
+            )
+            '''
+        )
+        c.execute(
+            '''
+            CREATE INDEX IF NOT EXISTS idx_neuron_bodies_001 ON neuron_bodies (
+                species_id
+            )
+            '''
+        )
+
+
 
 
 
