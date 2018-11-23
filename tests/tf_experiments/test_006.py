cfe314a93307326e2b3f7ee558e468a077084dcf
Maks Kozharin <maksko@yandex-team.ru>
2018-11-23 16:58:07 +0300

    experiments with tf (add summaries for using tensorboard)

--- tests/tf_experiments/test_006.py
+++ tests/tf_experiments/test_006.py
@@ -9,10 +9,10 @@ import matplotlib.pyplot as plt
 graph = tf.get_default_graph()
 
 # Создадим входное значение
-input_value = tf.constant(1.0)
+input_value = tf.constant(1.0, name='input_value')
 
 # Создадим переменную
-weight = tf.Variable(0.8)
+weight = tf.Variable(0.8, name='weight')
 
 # Создадим выходное значение
 output_value = weight * input_value
@@ -25,11 +25,14 @@ desired_output_value = tf.constant(2.0)
 loss = (output_value - desired_output_value) ** 2  # Функция ошибки
 
 tf.summary.scalar('loss', loss)
+optim = tf.train.GradientDescentOptimizer(learning_rate=0.025)  # Оптимизатор
+
+grads_and_vars = optim.compute_gradients(loss)
+tf.summary.scalar('grads', grads_and_vars[0][0])
+tf.summary.scalar('vars', grads_and_vars[0][1])
+
 merged_summary = tf.summary.merge_all()
 summary_writer = tf.summary.FileWriter('/home/maksko/-/Temp/001', graph)
-
-optim = tf.train.GradientDescentOptimizer(learning_rate=0.025)  # Оптимизатор
-grads_and_vars = optim.compute_gradients(loss)
 
 # Инициализируем переменные
 init = tf.global_variables_initializer()
@@ -39,6 +42,8 @@ sess.run(init)
 # train_step = tf.train.GradientDescentOptimizer(0.025).minimize(loss)
 train_step = optim.minimize(loss)
 for i in range(100):
+
+    ooo = sess.run(grads_and_vars)
 
     summary = sess.run(merged_summary)
     summary_writer.add_summary(summary, i)
--- tests/tf_experiments/test_007.py
+++ tests/tf_experiments/test_007.py
@@ -1,18 +1,16 @@
 ##################################################################
 ## http://wiki.programstore.ru/python-tensorflow-dlya-chajnikov-chast-1/
 #
-# Пример использования обучения tensorflow-графа нейросети на простом наборе тестовых данных.
+# Обучение функции OR.
 # Граф: два нейрона первого слоя и один нейрон второго
-#       у нейронов первого слоя есть смещение
-#       передаточная функция -- relu
+#       у каждого нейрона есть смещение
+#       передаточная функция -- сигмоида
 #
-# Требуемое значение на выходе: (a xor b)
-# Количество итераций: 200
-#
-# Для визуализации предполагаем использовать tensorboard
+# Требуемое значение на выходе: 2
+# Количество итераций: 100
 
-import datetime
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 def variable_summaries(scope, var):
@@ -28,16 +26,14 @@ def variable_summaries(scope, var):
         tf.summary.scalar('stddev', stddev)
         tf.summary.scalar('max', tf.reduce_max(var))
         tf.summary.scalar('min', tf.reduce_min(var))
-        # tf.summary.histogram('histogram', var)
+        tf.summary.histogram('histogram', var)
 
 
-def tensor_to_list(t):
-    if t is None:
-        return []
+def expand_tensor(t):
     l = []
     if t.shape.ndims > 0:
         for subt in (t[i] for i in range(t.shape.dims[0])):
-            l.extend(tensor_to_list(subt))
+            l.extend(expand_tensor(subt))
     else:
         return [t]
     return l
@@ -60,32 +56,14 @@ desired_value = tf.placeholder(dtype=tf.float32, shape=[None], name="desired_val
 # веса синапсов (2 входа * 2 нейрона + 2 входа на нейрон второй сети)
 weight_lay_1 = tf.Variable(weight_initializer([2, 2]), name="weight_lay_1")
 weight_lay_2 = tf.Variable(weight_initializer([2, 1]), name="weight_lay_2")
-# смещения нейронов (только для первой сети -- 2 нейрона)
+
 bias_lay_1 = tf.Variable(bias_initializer([2]), name="bias_lay_1")
-# # bias_lay_2 = tf.Variable(bias_initializer([1]), name="bias_lay_2")
-
-# weight_lay_1 = tf.Variable([[1, 1], [1, 1]], name="weight_lay_1", dtype=tf.float32)
-# # weight_lay_2 = tf.Variable([[1], [-2]], name="weight_lay_2", dtype=tf.float32)
-# weight_lay_2 = tf.Variable([1, -2], name="weight_lay_2", dtype=tf.float32)
-# weight_lay_2 = tf.reshape(weight_lay_2, shape=(2, 1))
-# bias_lay_1 = tf.Variable([0, -1], name="bias_lay_1", dtype=tf.float32)
-# # bias_lay_2 = tf.Variable([0], name="bias_lay_2", dtype=tf.float32)
-# # bias_lay_2 = tf.Variable(0, name="bias_lay_2", dtype=tf.float32)
-
-# # weight_lay_1 = tf.Variable([[1.178932, 1.072376], [0.926467, 1.3678287]], name="weight_lay_1", dtype=tf.float32)
-# weight_lay_2 = tf.Variable([0.8737834, -2.3245216], name="weight_lay_2", dtype=tf.float32)
-# # bias_lay_1 = tf.Variable([0, -1], name="bias_lay_1", dtype=tf.float32)
-#
-# weight_lay_1 = tf.constant([[1, 1], [1, 1]], name="weight_lay_1", dtype=tf.float32)
-# # weight_lay_2 = tf.constant([1, -2], name="weight_lay_2", dtype=tf.float32)
-# bias_lay_1 = tf.constant([0, -1], name="bias_lay_1", dtype=tf.float32)
-#
-# weight_lay_2 = tf.reshape(weight_lay_2, shape=(2, 1))
+bias_lay_2 = tf.Variable(bias_initializer([1]), name="bias_lay_2")
 
 variable_summaries('weight_lay_1', weight_lay_1)
 variable_summaries('weight_lay_2', weight_lay_2)
 variable_summaries('bias_lay_1', bias_lay_1)
-# variable_summaries('bias_lay_2', bias_lay_2)
+variable_summaries('bias_lay_2', bias_lay_2)
 
 # weight_lay_1 = [
 #     [tf.Variable(0), tf.Variable(0)],   # нейрон 1 (первый слой)
@@ -102,22 +80,13 @@ variable_summaries('bias_lay_1', bias_lay_1)
 # выходное значение
 # a = tf.nn.relu(tf.matmul(input_value, weight_lay_1) )
 # b = tf.nn.relu(tf.matmul(a          , weight_lay_2) )
-a1 = tf.matmul(input_value, weight_lay_1)
-a2 = a1 + bias_lay_1
-a3 = tf.nn.relu(a2)
-a = a3 #tf.reshape(a3, shape=(2))
-b1 = tf.matmul(a, weight_lay_2)
-b1 = tf.reshape(b1, shape=[tf.shape(b1)[0]])
-b2 = b1 #+ bias_lay_2
-b3 = tf.nn.relu(b2)
-b = b3
-# a = tf.nn.relu(tf.matmul(input_value, weight_lay_1) + bias_lay_1)
-# b = tf.nn.relu(tf.matmul(a          , weight_lay_2) + bias_lay_2)
+a = tf.nn.relu(tf.matmul(input_value, weight_lay_1) + bias_lay_1)
+b = tf.nn.relu(tf.matmul(a          , weight_lay_2) + bias_lay_2)
 # a = tf.sigmoid(tf.matmul(input_value, weight_lay_1) + bias_lay_1)
 # b = tf.sigmoid(tf.matmul(a          , weight_lay_2) + bias_lay_2)
 
-# tf.summary.histogram('a', a)
-# tf.summary.histogram('b', b)
+tf.summary.histogram('a', a)
+tf.summary.histogram('b', b)
 
 # a = tf.sigmoid(weight_lay_1[0] * input_value + bias_lay_1)
 # b = tf.sigmoid(weight_lay_1[1] * input_value + bias_lay_1)
@@ -130,22 +99,19 @@ sess = tf.Session()
 
 # Создаем оптимизиатор
 #desired_output_value = inout_value[2]
-# loss1 = tf.squared_difference(output_value, desired_value)
-# loss2 = tf.reduce_mean(loss1)
-# loss = loss2
 loss = tf.reduce_mean(tf.squared_difference(output_value, desired_value))
 variable_summaries('loss', loss)
 # loss = (output_value - desired_output_value) ** 2  # Функция ошибки
 optim = tf.train.GradientDescentOptimizer(learning_rate=0.25)  # Оптимизатор
 grads_and_vars = optim.compute_gradients(loss)
 for gav in grads_and_vars:
-    for o in tensor_to_list(gav[0]):
+    for o in expand_tensor(gav[0]):
         tf.summary.scalar('grad-' + gav[1].name + '-' + o.name, o)
-    for o in tensor_to_list(gav[1]):
+    for o in expand_tensor(gav[1]):
         tf.summary.scalar('var-' + gav[1].name + '-'  + o.name, o)
 
-#    ooo0 = tensor_to_list(gav[0])
-#    ooo1 = tensor_to_list(gav[1])
+#    ooo0 = expand_tensor(gav[0])
+#    ooo1 = expand_tensor(gav[1])
 #
 #    tf.summary.tensor_summary('grad-' + gav[1].name, gav[0])
 #    tf.summary.tensor_summary('var-' + gav[1].name, gav[1])
@@ -176,11 +142,10 @@ data_in = [
     [0, 0]
 ]
 data_out = [0, 1, 1, 0]
-# data_out = [[0], [1], [1], [0]]
 
 merged = tf.summary.merge_all()
 
-file_writer = tf.summary.FileWriter('/home/maksko/-/Temp/001/' + str(datetime.datetime.now()), graph)
+file_writer = tf.summary.FileWriter('/home/maksko/-/Temp/001', graph)
 
 # Обучаем
 # train_step = tf.train.GradientDescentOptimizer(0.025).minimize(loss)
@@ -189,25 +154,6 @@ feed_data={input_value: data_in, desired_value: data_out}
 for i in range(200):
     summary = sess.run(merged, feed_dict=feed_data)
     file_writer.add_summary(summary, i)
-
-    # cur_a1    = sess.run(   a1, feed_dict=feed_data)
-    # cur_a2    = sess.run(   a2, feed_dict=feed_data)
-    # cur_a3    = sess.run(   a3, feed_dict=feed_data)
-    # cur_a     = sess.run(   a , feed_dict=feed_data)
-    # cur_b1    = sess.run(   b1, feed_dict=feed_data)
-    # cur_b2    = sess.run(   b2, feed_dict=feed_data)
-    # cur_b3    = sess.run(   b3, feed_dict=feed_data)
-    # cur_b     = sess.run(   b , feed_dict=feed_data)
-    # cur_loss  = sess.run(loss , feed_dict=feed_data)
-    # cur_loss1 = sess.run(loss1, feed_dict=feed_data)
-    # cur_loss2 = sess.run(loss2, feed_dict=feed_data)
-
-
-
-
-
-
-
 
     sess.run(train_step, feed_dict=feed_data)
     # x.append(i)
