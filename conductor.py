536985c8a4648bdf11f0471bca3e6d618f54892a
maksko <tttmaksko@mail.ru>
2018-12-16 17:10:12 +0300

    small fixes in conductor

--- conductor.py
+++ conductor.py
@@ -7,12 +7,11 @@
 from engine import Engine
 
 class Conductor:
-    def __init__(self, engine : Engine):
-        self._engine = engine
+    def __init__(self):
         # очереди нейросетей. Содержат кортежи с данными вида:
         #   (id, adaptability)
         # adaptability=0 означает наименее приспособленную сеть
-        self._nn_queue_active = engine.db.get_alive_species()
+        self._nn_queue_active = Engine.db().get_alive_species()
         self._nn_queue_pending = []
         if len(self._nn_queue_active) == 0:
             self._nn_queue_active = [(0, 0)]
@@ -21,7 +20,7 @@ class Conductor:
         if len(self._nn_queue_active) == 0:
             self._nn_queue_pending, self._nn_queue_active = self._nn_queue_active, self._nn_queue_pending
             self._nn_queue_active.sort(key=lambda x: x[1], reverse=True)
-            max_len = self._engine.config.alive_neural_network_queue_len()
+            max_len = Engine.config().alive_neural_network_queue_len()
             # todo: при удалении сетей из очереди их надо помечать неактивными в базе данных
             if len(self._nn_queue_active) > max_len:
                 self._nn_queue_active = self._nn_queue_active[:max_len]
